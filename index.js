0c070bc646ee6a0dc0a6066e5418c4b38c3a8022
Juanlu <juanlucg99.jl@gmail.com>
2022-03-22 22:52:27 +0100

    v1

---
+++ index.js
@@ -0,0 +1,39 @@
+/*Si clicamos en el botón del sol, borrarémos la clase css dark-mode del div 
+con id page y se aplicará el estilo active al sol*/
+document.getElementById('id-sun').onclick = function(){
+    document.getElementById('page').classList.remove('dark-mode')
+    document.getElementById('id-moon').classList.remove('active')
+    this.classList.add('active')
+  }
+  /*Si clicamos en el botón de la luna, añadiremos la clase css dark-mode del div 
+  con id page y se aplicará el estilo active a la luna*/
+  document.getElementById('id-moon').onclick = function(){
+    document.getElementById('page').classList.add('dark-mode')
+    document.getElementById('id-sun').classList.remove('active')
+    this.classList.add('active')
+  }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onmouseenter = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
